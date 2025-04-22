1ddc5937ebfab545f8d2afe5d167122fe37e2d82
Mayur Rathod <mdridrdrr@gmail.com>
2025-04-22 09:35:49 +0530

    Implement command palate for search

--- app/components/Sidebar/Search.tsx
+++ app/components/Sidebar/Search.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FiCommand, FiSearch } from "react-icons/fi";
+import { CommandMenu } from "./CommandMenu";
 
 const Search = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
@@ -10,11 +14,16 @@ const Search = () => {
           type="text"
           placeholder="Search"
           className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
+          onFocus={(e)=>{
+            e.target.blur();
+            setOpen(true)
+          }}
         />
         <span className="p-1 text-xs flex gap-0.5 items-center shadow bg-stone-50 rounde absolute right-1.5 top-1/2 -translate-y-1/2">
           <FiCommand />
         </span>
       </div>
+      <CommandMenu open={open} setOpen={setOpen}/>
     </>
   );
 };
