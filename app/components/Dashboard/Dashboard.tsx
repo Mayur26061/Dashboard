7d822c65c302afad50f61b8c1cb729b6c39d660b
Mayur Rathod <mdridrdrr@gmail.com>
2025-04-23 23:29:20 +0530

    Adding Topbar for dashboard

--- app/components/Dashboard/Dashboard.tsx
+++ app/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Topbar from "./Topbar";
 
 const Dashboard = () => {
-  return <div className="bg-white rounded-lg pb-3 shadow h-[200vh]"></div>;
+  return <div className="bg-white rounded-lg pb-3 shadow h-[200vh]">
+    <Topbar/>
+  </div>;
 };
 
 export default Dashboard;
